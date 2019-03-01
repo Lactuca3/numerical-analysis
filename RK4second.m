a60b7ea4ce2be6554d3beca0d5f6ffc096936861
Lulwa Alkhalifa <46845566+Lactuca3@users.noreply.github.com>
2019-03-01 08:49:35 +0000

    Added RLC_script and RK4second

---
+++ RK4second.m
@@ -0,0 +1,9 @@
+function [x2,y2] = RK4second(x,y,t)
+%RK4second implements the fourth-order Runge-Kutta 3/8 algorithm for a
+%single time-step, taking input xi,yi,and ti, and returning xi+1 and yi+1
+%Detailed explanation goes here
+
+
+
+end
+
---
+++ RLC_script.m
@@ -0,0 +1,8 @@
+R=250; %set resistance (R) = 250?
+C=3*10.^-6; %set capacitance (C) = 3?F
+L=650*10.^-3; %set inductance (L) = 650mH
+V_in=5; %expression describing input signal 
+
+z'=V_in
+
+
